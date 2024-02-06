f1c25f6ec147da8c3c17d42a8327beb2255f61f7
Cesar <CesarCordova.developer@gmail.com>
2024-02-05 22:24:52 -0800

    pyrhon test

---
+++ hello.py
@@ -0,0 +1,54 @@
+print('hello, world!')
+var = 10
+print(var)
+
+#variables
+
+x = 6
+y= 7
+
+#operations
+sum = x + y
+print(sum)
+
+#string operations
+name = "cesar"
+greeting = "hello, " + name
+print(greeting)
+
+#lists and arrays
+numbers = [1, 2, 3, 4, 5]
+
+#loops
+for i in numbers:
+    print(i)
+
+age = 20
+if age > 21:
+  print("you can drink")
+else:
+  print("you can't drink")
+
+#functions
+def greet(name):
+  print("hello, " + name)
+
+greet("cesar")
+
+example = 1
+example2 = '2'
+
+print(example + int(example2))
+
+#function to calculate the power of a number
+def power(base, exponent):
+  result = base ** exponent
+  return result
+
+print(power(2, 3))
+
+#function to multiply two numbers
+def multiply(x, y):
+  return x * y
+
+print(multiply(2, 3))
